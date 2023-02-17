35240ec20946ceed4d309d10e2834fcd2040a1db
TheDevick <erick@bilhalba.com.br>
2023-02-17 22:37:31 +0000

    feat: use Forms in Login Route

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
+use App\Form\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/auth', name: 'app.auth.')]
 class AuthController extends AbstractController
 {
-    #[Route(path: '/login', name: 'login.view', methods: 'GET')]
-    public function loginView(): Response
+    #[Route(path: '/login', name: 'login.view', methods: 'GET|POST')]
+    public function loginView(Request $request): Response
     {
-        return $this->render('auth/login.html.twig');
+        $form = $this->createForm(LoginFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var \App\Entity\Client $client */
+            $client = $form->getData();
+            $name = $client->getName() ?? 'NULL';
+
+            $this->addFlash('success', sprintf('Login Successful! Logged as %s', $name));
+
+            return $this->redirectToRoute('app.dashboard.index');
+        }
+
+        return $this->render('auth/login.html.twig', ['loginForm' => $form->createView()]);
     }
 }
