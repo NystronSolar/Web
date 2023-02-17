11898eac9d32560280ced04bc529c5a491f78b20
TheDevick <erick@bilhalba.com.br>
2023-02-17 20:12:57 +0000

    feat: add CPF Assert

---
+++ src/Validator/CPFValidator.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+class CPFValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof CPF) {
+            throw new UnexpectedTypeException($constraint, CPF::class);
+        }
+
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!is_string($value)) {
+            throw new UnexpectedValueException($value, 'string');
+        }
+
+        if (!$this->isValid($value)) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
+    }
+
+    /**
+     * Checks if a CPF is valid or not.
+     */
+    private function isValid(string $cpf): bool
+    {
+        $cpf = preg_replace('/[^0-9]/is', '', $cpf);
+
+        if (11 != strlen($cpf)) {
+            return false;
+        }
+
+        if (preg_match('/(\d)\1{10}/', $cpf)) {
+            return false;
+        }
+
+        for ($t = 9; $t < 11; ++$t) {
+            for ($d = 0, $c = 0; $c < $t; ++$c) {
+                $d += $cpf[$c] * (($t + 1) - $c);
+            }
+            $d = ((10 * $d) % 11) % 10;
+            if ($cpf[$c] != $d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
