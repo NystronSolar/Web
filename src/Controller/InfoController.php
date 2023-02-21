a5f15073584c1a077eb8a0023f771bbf83e89e90
TheDevick <erick@bilhalba.com.br>
2023-02-21 05:24:28 +0000

    fix: run PHP CS Fixer

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -15,30 +15,30 @@ class InfoController extends AbstractController
         $credits = [
             'Icons' => [
                 [
-                    "Link" => "https://thenounproject.com/icon/home-1560062/",
-                    "Title" => "Home",
-                    "AuthorLink" => "https://thenounproject.com/il.capitano/",
-                    "Author" => "il Capitano",
-                    "CorporationLink" => "https://thenounproject.com/",
-                    "Corporation" => "Noun Project"
+                    'Link' => 'https://thenounproject.com/icon/home-1560062/',
+                    'Title' => 'Home',
+                    'AuthorLink' => 'https://thenounproject.com/il.capitano/',
+                    'Author' => 'il Capitano',
+                    'CorporationLink' => 'https://thenounproject.com/',
+                    'Corporation' => 'Noun Project',
                 ],
                 [
-                    "Link" => "https://thenounproject.com/icon/electric-bill-2864536/",
-                    "Title" => "Electric Bill",
-                    "AuthorLink" => "https://thenounproject.com/auliausu/",
-                    "Author" => "Aulia Rahman",
-                    "CorporationLink" => "https://thenounproject.com/",
-                    "Corporation" => "Noun Project"
+                    'Link' => 'https://thenounproject.com/icon/electric-bill-2864536/',
+                    'Title' => 'Electric Bill',
+                    'AuthorLink' => 'https://thenounproject.com/auliausu/',
+                    'Author' => 'Aulia Rahman',
+                    'CorporationLink' => 'https://thenounproject.com/',
+                    'Corporation' => 'Noun Project',
                 ],
                 [
-                    "Link" => "https://thenounproject.com/icon/solar-powered-home-1271326/",
-                    "Title" => "Solar Powered Home",
-                    "AuthorLink" => "https://thenounproject.com/symbolon/",
-                    "Author" => "Symbolon",
-                    "CorporationLink" => "https://thenounproject.com/",
-                    "Corporation" => "Noun Project"
-                ]
-            ]
+                    'Link' => 'https://thenounproject.com/icon/solar-powered-home-1271326/',
+                    'Title' => 'Solar Powered Home',
+                    'AuthorLink' => 'https://thenounproject.com/symbolon/',
+                    'Author' => 'Symbolon',
+                    'CorporationLink' => 'https://thenounproject.com/',
+                    'Corporation' => 'Noun Project',
+                ],
+            ],
         ];
 
         return $this->render('info/credits.html.twig', ['credits' => $credits]);
