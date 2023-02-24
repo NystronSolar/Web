eee3bd818b84f210b0907d6b807d65207da1e4d1
TheDevick <erick@bilhalba.com.br>
2023-02-24 08:17:53 +0000

    feat: create Endpoints to Upload Generation Files

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use NystronSolar\GrowattSpreadsheet\Reader\ReaderFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,8 +28,19 @@ class AdminController extends AbstractController
     #[Route(path: '/upload/generation', name: 'upload.generation.action', methods: 'POST')]
     public function uploadGenerationAction(Request $request): Response
     {
+        /** @var UploadedFile $report */
         $report = $request->files->all()['report'];
 
-        return new Response("OK!");
+        $reportFolder = $this->getParameter('kernel.project_dir').'/var/uploads/';
+        $reportFileName = $report->getClientOriginalName();
+        $report->move($reportFolder, $reportFileName);
+
+        $reader = ReaderFactory::fromFile($reportFolder.$reportFileName);
+        $reportSpreadsheet = $reader->search();
+
+        $filesystem = new Filesystem();
+        $filesystem->remove($reportFolder.$reportFileName);
+
+        dd($reportSpreadsheet);
     }
 }
