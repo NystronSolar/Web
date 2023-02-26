c5254585bbd93b321a64fe5891109bdb4b613694
TheDevick <erick@bilhalba.com.br>
2023-02-26 06:54:35 +0000

    feat: add Unique Entity to Client

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -5,11 +5,13 @@ namespace App\Entity;
 use App\Repository\ClientRepository;
 use App\Validator as AppAssert;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Client implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -161,13 +163,13 @@ class Client implements UserInterface, PasswordAuthenticatedUserInterface
     public function toArray(bool $strings = false)
     {
         return [
-            "Id" => $this->getId(),
-            "Email" => $this->getEmail(),
-            "Name" => $this->getName(),
-            "CPF" => $this->getCPF(),
-            "Roles" => $strings ? $this->getRolesToString() : $this->getRoles(),
-            "Password" => $this->getPassword(),
-            "GrowattName" => $this->getGrowattName(),
+            'Id' => $this->getId(),
+            'Email' => $this->getEmail(),
+            'Name' => $this->getName(),
+            'CPF' => $this->getCPF(),
+            'Roles' => $strings ? $this->getRolesToString() : $this->getRoles(),
+            'Password' => $this->getPassword(),
+            'GrowattName' => $this->getGrowattName(),
         ];
     }
 }
